82d6879e86fa11444d48d1b7207a0c412a1e1f88
nevermyuk <zequanong@gmail.com>
2019-11-21 21:09:04 +0800

    stuck at step2. creating button for every list item

--- part2/2c/data-for-countries/src/index.js
+++ part2/2c/data-for-countries/src/index.js
@@ -1,12 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React,{ useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import axios from 'axios'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const MultiDisplay = ({results}) => {
+            return  (
+                results.map((country => {
+                return (
+                <div key = {country.alpha3code}>
+                <p key = {country.name}><button onClick={handleShowChange}>SHOW</button></p>
+                <p>{country.capital}</p>
+                <p>Population {country.population}</p>
+                <h2>languages</h2>
+                
+                <ul>{country.languages.map((language)=>{
+        
+                return (<li key={language.iso639_1}>{language.name}</li>)
+            })}</ul>
+                <img src = {country.flag} alt = {country.name} height="100" width="100"/> 
+                </div>
+                )}))
+            )}
+
+
+const Display = ({results}) => {
+    return  (<div>
+        <h1>{results[0].name}</h1>
+        <p>{results[0].capital}</p>
+        <p>Population {results[0].population}</p>
+        <h2>languages</h2>
+        
+        <ul>{results[0].languages.map((language)=>{
+
+        return (<li key={language.iso639_1}>{language.name}</li>)
+    })}</ul>
+        <img src = {results[0].flag} alt = {results[0].name} height="100" width="100"/></div>)
+    }
+
+
+
+const App = () => {
+    const [countries,setCountries] = useState([])
+    const [newQuery, setNewQuery] = useState('')
+    const [show , setShow] = useState([])
+
+    const handleShowChange = () => setShow([])
+       
+    const handleQueryChange = (event) => setNewQuery(event.target.value) 
+    useEffect(()=> {
+
+        axios.get('https://restcountries.eu/rest/v2/all')
+        .then((response) => setCountries(response.data))
+    
+    },[])
+
+
+    const results = !newQuery ? [] : countries.filter(country => country.name.toLowerCase().includes(newQuery.toLowerCase())) 
+    console.log(results)
+    return (
+    <div>
+        Find countries <input value = {newQuery} onChange = {handleQueryChange}/>
+        {results.length > 10 ? 
+        <p>Too many matches,specify another filter</p> 
+        : (results.length === 1) ?
+       
+        <Display results = {results} />: <MultiDisplay />}
+    </div>
+        
+        
+        
+
+    )
+}
+
+
+
+ReactDOM.render(<App />,document.getElementById('root'))
