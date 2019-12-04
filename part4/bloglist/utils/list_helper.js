3be9be75e2756d87a0da9a1db7353504d1f89424
nevermyuk <zequanong@gmail.com>
2019-12-04 21:36:57 +0800

    4.4: helper functions and unit tests, step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,14 @@
-const dummy = () => 1
+const dummy = (blogs) => 1
+//Receives an array of blogs and return 1
 
+
+
+const totalLikes = (blogs) => {
+// Returns total likes of blogs
+  const reducer = (totalLikes,blog) => blog.likes+totalLikes
+  return blogs.reduce(reducer,0)
+}
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
