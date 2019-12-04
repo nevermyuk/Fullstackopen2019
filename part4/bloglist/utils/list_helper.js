603d10c017534012654826c1515f46d27fe0ddfc
nevermyuk <zequanong@gmail.com>
2019-12-04 21:58:35 +0800

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,20 @@ const totalLikes = (blogs) => {
   const reducer = (totalLikes,blog) => blog.likes+totalLikes
   return blogs.reduce(reducer,0)
 }
+
+/* favouriteBlog function finds out which blog has most likes.
+If there are many top favorites,
+it is enough to return one of them.*/
+const favouriteBlog = (blogs) => {
+ const mostLikedNumber = Math.max(...blogs.map(blog => blog.likes))
+ const mostLikedBlog = blogs.find(blog => blog.likes === mostLikedNumber)
+    return mostLikedBlog
+}
+
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
+
